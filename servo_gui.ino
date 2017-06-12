6c636b813c2e5b9ca76e0a6ad2aa0ffb0a8f92c0
vsai97 <vsai78677@gmail.com>
2017-06-12 17:13:21 +0530

    Create servo_gui.ino

--- servo_gui.ino
+++ servo_gui.ino
@@ -1,38 +1,78 @@
+// OPTICAL LASER BIO METER target angualtion
+//AUTHOR: Chinmaie Gorla 
+//DESCRIPTION: To rotate the target(both clockwise and anti clockwise upto 45 degrees)
+//attested to the servo motor mounted over 
+//the head clamp of optical Laser Bio Meter which is used to diagnose the IOL 
+//power of Cataract patients
+// Date of Latest Build: 12-06-2017:05:00 PM
+
 #include<Servo.h>
-Servo m1servo;
+Servo m1servo;//Initialize servo to borrow servo library
 void setup(){
-Serial.begin(9600); // Initialize Serial Communitication
-m1servo.attach(9);} // attaches the servo on pin 9 to the servo object
+Serial.begin(9600); // Initialize Serial Communiticationat 9600 bits per second
+m1servo.attach(9); // attaches the servo on pin 9 to the servo object
+m1servo.write(105);}//initialise sero angle to mean position as measured
+ 
  void loop(){
- if (Serial.available() > 0) { int data = Serial.read();
- switch (data) {
- case '1': //Number 1 is received
- m1servo.write(5); //tell servo to go to position in variable 'data'
+ if (Serial.available() > 0) {
+ int data = Serial.read();
+ switch (data) { // Value in 'data' is being recieved from GUI through Serial Port
+ case 'a': //Alphabet is recieved from GUI which iterates through switch case with variable loaded under 'data'
+ m1servo.write(150); //shift to right extreme
  break;
- case '2' : //Number 2 is received,
- m1servo.write(10);
+ case 'b' : 
+ m1servo.write(145);
  break;
- case '3' : //Number 3 is received,
- m1servo.write(15);
+ case 'c' : 
+ m1servo.write(140);
  break;
-case '4' : //Number 4 is received,
- m1servo.write(20);
+case 'd' : 
+ m1servo.write(135);
  break;
- case '5': //Number 5 is received
- m1servo.write(25);
+ case 'e': 
+ m1servo.write(130);
  break;
- case '6' : //Number 6 is received,
- m1servo.write(30);
+ case 'f' : 
+ m1servo.write(125);
  break;
- case '7' : //Number 7 is received,
- m1servo.write(35);
+ case 'g' : 
+ m1servo.write(120);
  break;
- case '8' : //Number 8 is received,
- m1servo.write(40);
+ case 'h' : 
+ m1servo.write(115);
  break;
- case '9': //Number 9 is received
- m1servo.write(45);
+ case 'i': 
+ m1servo.write(110);
+ break;
+ case 'j': 
+ m1servo.write(105);//Shift to mean position 
+ break;
+ case 'k': 
+ m1servo.write(100);
+ break;
+ case 'l': 
+ m1servo.write(95);
+ break;
+ case 'm': 
+ m1servo.write(90);
+ break;
+ case 'n': 
+ m1servo.write(85);
+ break;
+ case 'o': 
+ m1servo.write(80);
+ break;
+ case 'p': 
+ m1servo.write(75);
+ break;
+ case 'q': 
+ m1servo.write(70);
+ break;
+ case 'r': 
+ m1servo.write(65);
+ break;
+ case 's': 
+ m1servo.write(60);//Shift to left extreme
  break;
  default :
  break; } } }
-
