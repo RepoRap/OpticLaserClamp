ef8af822deee6a9c24f82c2c3f1cf21f381e0c16
Chinmaie <chinmaie.gorla@gmail.com>
2017-06-12 17:24:57 +0530

    Create servo_gui.ino

--- servo_gui.ino
+++ servo_gui.ino
@@ -4,7 +4,7 @@
 //attested to the servo motor mounted over 
 //the head clamp of optical Laser Bio Meter which is used to diagnose the IOL 
 //power of Cataract patients
-// Date of Latest Build: 12-06-2017:05:00 PM
+// Date of Latest Build: 12-06-2017 05:24 PM
 
 #include<Servo.h>
 Servo m1servo;//Initialize servo to borrow servo library
@@ -17,61 +17,61 @@ m1servo.write(105);}//initialise sero angle to mean position as measured
  if (Serial.available() > 0) {
  int data = Serial.read();
  switch (data) { // Value in 'data' is being recieved from GUI through Serial Port
- case 'a': //Alphabet is recieved from GUI which iterates through switch case with variable loaded under 'data'
+ case 's': //Alphabet is recieved from GUI which iterates through switch case with variable loaded under 'data'
  m1servo.write(150); //shift to right extreme
  break;
- case 'b' : 
+ case 'r' : 
  m1servo.write(145);
  break;
- case 'c' : 
+ case 'q' : 
  m1servo.write(140);
  break;
-case 'd' : 
+case 'p' : 
  m1servo.write(135);
  break;
- case 'e': 
+ case 'o': 
  m1servo.write(130);
  break;
- case 'f' : 
+ case 'n' : 
  m1servo.write(125);
  break;
- case 'g' : 
+ case 'm' : 
  m1servo.write(120);
  break;
- case 'h' : 
+ case 'l' : 
  m1servo.write(115);
  break;
- case 'i': 
+ case 'k': 
  m1servo.write(110);
  break;
  case 'j': 
  m1servo.write(105);//Shift to mean position 
  break;
- case 'k': 
+ case 'i': 
  m1servo.write(100);
  break;
- case 'l': 
+ case 'h': 
  m1servo.write(95);
  break;
- case 'm': 
+ case 'g': 
  m1servo.write(90);
  break;
- case 'n': 
+ case 'f': 
  m1servo.write(85);
  break;
- case 'o': 
+ case 'e': 
  m1servo.write(80);
  break;
- case 'p': 
+ case 'd': 
  m1servo.write(75);
  break;
- case 'q': 
+ case 'c': 
  m1servo.write(70);
  break;
- case 'r': 
+ case 'b': 
  m1servo.write(65);
  break;
- case 's': 
+ case 'a': 
  m1servo.write(60);//Shift to left extreme
  break;
  default :
